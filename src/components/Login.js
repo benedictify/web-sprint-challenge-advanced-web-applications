7d5b3cc3bfe729ae8be80dd085f33d0b7edf253a
Benedict Schurwanz <benedictify@gmail.com>
2021-11-12 14:13:58 -0600

    add form with fields, styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Login = () => {
+const Login = (props) => {
+	const { setToken } = props;
+	const [user, setUser] = useState({
+		name: 'Lambda',
+		pw: 'School'
+	})
 
-	return (<ComponentContainer>
-		<ModalContainer>
-			<h1>Welcome to Blogger Pro</h1>
-			<h2>Please enter your account information.</h2>
-		</ModalContainer>
-	</ComponentContainer>);
+	const { push } = useHistory();
+
+	const loginSubmit = (event) => {
+		event.preventDefault();
+
+		push('/view')
+	}
+
+	const inputChange = () => {
+
+	}
+
+	return (
+		<ComponentContainer>
+			<ModalContainer>
+				<h1>Welcome to Blogger Pro</h1>
+				<h2>Please enter your account information.</h2>
+			</ModalContainer>
+
+			<FormGroup>
+				<Label>Username
+					<Input
+						type="text"
+						name="username"
+						value={user.name}
+						onChange={inputChange}
+					/>
+				</Label>
+				<Label>Password
+					<Input
+						type="password"
+						name="password"
+						value={user.pw}
+						onChange={inputChange}
+					/>
+				</Label>
+				<Button onClick={loginSubmit}>Log in</Button>
+			</FormGroup>
+
+		</ComponentContainer>);
 }
 
 export default Login;
@@ -22,36 +62,33 @@ export default Login;
 //6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
 
 const ComponentContainer = styled.div`
-    height: 70%;
-    justify-content: center;
-    align-items: center;
-    display:flex;
+	height: 70%;
+	justify-content: center;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
 `
-
 const ModalContainer = styled.div`
-    width: 500px;
-    background: white;
-    padding: 2rem;
-    text-align: center;
+	width: 500px;
+	background: white;
+	padding: 2rem;
+	text-align: center;
+	margin-top: 7.5rem;
 `
-
 const Label = styled.label`
-    display: block;
-    text-align: left;
-    font-size: 1.5rem;
+	display: block;
+	text-align: left;
+	font-size: 1.5rem;
 `
-
 const FormGroup = styled.form`
-    padding:1rem;
+	padding:1rem;
 `
-
 const Input = styled.input`
-    font-size: 1rem;
-    padding: 1rem 0;
-    width:100%;
+	font-size: 1rem;
+	padding: 1rem 0;
+	width: 100%;
 `
-
 const Button = styled.button`
-    padding:1rem;
-    width: 100%;
+	padding: 1rem;
+	width: 100%;
 `
