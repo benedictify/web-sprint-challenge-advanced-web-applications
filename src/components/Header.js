4fae96a2b6fd77cfa23601ab831bfb1697a99fa7
Benedict Schurwanz <benedictify@gmail.com>
2021-11-12 13:14:04 -0600

    formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,27 +18,27 @@ const Header = () => {
 export default Header;
 
 const HeaderStyle = styled.div`
-    padding: 1em;
-    background-color: #daa1ac;
-    display:flex;
-    align-items: center;
-    justify-content: space-between;
+	padding: 1em;
+	background-color: #daa1ac;
+	display:flex;
+	align-items: center;
+	justify-content: space-between;
 
-    p {
-        font-weight: bold;
-        font-size: 1.5em;
-    }
+	p {
+		font-weight: bold;
+		font-size: 1.5em;
+	}
 `
 
 const MenuStyle = styled.ul`
   li {
-        display: inline-block;
-        padding: 0.3rem 1rem;
-        
-        a {
-            text-decoration: none;
-            color: black;
-            font-size: 1em;
-        }
+		display: inline-block;
+		padding: 0.3rem 1rem;
+		
+		a {
+			text-decoration: none;
+			color: black;
+			font-size: 1em;
+		}
     }
 `
