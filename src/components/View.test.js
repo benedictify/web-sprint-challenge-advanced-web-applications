49298df3201db0508ea308dca9faf90d4951e980
Benedict Schurwanz <benedictify@gmail.com>
2021-11-12 18:55:45 -0600

    first view test

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -3,10 +3,37 @@ import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
 
 test("renders zero articles without errors", async () => {
-	
+	render(<View />);
 });
 
+const threeArticles = [
+	{
+		id: "abc",
+		headline: "headline",
+		author: "author",
+		summary: "summary",
+		body: "article body"
+	},
+	{
+		id: "abc",
+		headline: "headline",
+		author: "author",
+		summary: "summary",
+		body: "article body"
+	},
+	{
+		id: "abc",
+		headline: "headline",
+		author: "author",
+		summary: "summary",
+		body: "article body"
+	}
+]
+
 // test("renders three articles without errors", async ()=> {
+	
+	
+// 	render(<View />);
 // });
 
 //Task List
