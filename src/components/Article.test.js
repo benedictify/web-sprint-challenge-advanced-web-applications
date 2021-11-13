f3b3568c9318495ad301ef487830541236b0d107
Benedict Schurwanz <benedictify@gmail.com>
2021-11-12 18:20:47 -0600

    first test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,11 +5,22 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render } from '@testing-library/react';
+
+const initialArticle = {
+	id: "",
+	headline: "",
+	author: "",
+	summary: "",
+	body: ""
+};
 
 test('renders component without errors', () => {
+	render(<Article article={initialArticle} handleDelete={() => { }} handleEditSelect={() => { }} />);
 });
 
 // test('renders headline, author from the article when passed in through props', ()=> {
+
 // });
 
 // test('renders "Associated Press" when no author is given', ()=> {
