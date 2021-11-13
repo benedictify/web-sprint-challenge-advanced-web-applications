14fe4e46f86dd154d4f2de52aaba4dcb65ea684e
Benedict Schurwanz <benedictify@gmail.com>
2021-11-12 18:28:55 -0600

    second test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 const initialArticle = {
 	id: "",
@@ -19,15 +19,30 @@ test('renders component without errors', () => {
 	render(<Article article={initialArticle} handleDelete={() => { }} handleEditSelect={() => { }} />);
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
+test('renders headline, author from the article when passed in through props', ()=> {
+	const testArticle = {
+		id: "abc",
+		headline: "headline",
+		author: "author",
+		summary: "summary",
+		body: "article body"
+	}
 
-// });
+	render(<Article article={testArticle} />)
+
+	const headline = screen.queryByText(/headline/i);
+	const author = screen.queryByText(/author/i);
+	const summary = screen.queryByText(/summary/i);
+	const body = screen.queryByText(/article body/i);
+
+	expect(headline).toBeInTheDocument();
+	expect(author).toBeInTheDocument();
+	expect(summary).toBeInTheDocument();
+	expect(body).toBeInTheDocument();
+});
 
 // test('renders "Associated Press" when no author is given', ()=> {
 // });
 
 // test('executes handleDelete when the delete button is pressed', ()=> {
 // });
-
-//Task List:
-//1. Complete all above tests. Create test article data when needed.
